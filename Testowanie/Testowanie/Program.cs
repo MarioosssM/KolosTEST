9b97db9a2deb7fe8319e084b6fb8f68693a356c1
MarioosssM <mariopet27@gmail.com>
2016-05-15 22:58:35 +0200

    empty project with empty method and test

---
+++ Testowanie/Testowanie/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testowanie
+{
+    public class Program
+    {
+        public static void QuickSort()
+        {
+
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
