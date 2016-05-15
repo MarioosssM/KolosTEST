64ac410510b8e818194a185134edb2285426deec
MarioosssM <mariopet27@gmail.com>
2016-05-15 23:03:25 +0200

    add body to method QuickSort

--- Testowanie/Testowanie/Program.cs
+++ Testowanie/Testowanie/Program.cs
@@ -10,7 +10,26 @@ namespace Testowanie
     {
         public static void QuickSort(int[] tab, int s, int p)
         {
+            int i = p;
+            int j = s;
+            int pivot = tab[s];
+            while (i < j)
+            {
+                while (tab[i] < pivot) i++;
+                while (tab[j] > pivot) j--;
 
+                if (i <= j)
+                {
+                    int temp = tab[i];
+                    tab[i] = tab[j];
+                    tab[j] = temp;
+                    i++;
+                    j--;
+
+                }
+            }
+            if (p < j) QuickSort(tab, p, j);
+            if (i < s) QuickSort(tab, i, s);
 
         }
         static void Main(string[] args)
